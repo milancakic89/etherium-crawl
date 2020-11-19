04b1d7d4ee77495f16d5eda8bbe5efe5a88eee6e
Milan <milancakic89@gmail.com>
2020-11-19 21:54:50 +0100

    Basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
+import { ItemComponent } from './results/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ResultsComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
